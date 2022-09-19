d730b9d168fe3319ae8ed92b1fbe858418efa33f
Oscar Campohermoso <oscarcampohermoso8@gmail.com>
2022-09-18 23:55:49 -0400

    Commit Todo

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/index.html')
+})
+app.post('/quotes', (req, res) => {
+   console.log(req.body)
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
